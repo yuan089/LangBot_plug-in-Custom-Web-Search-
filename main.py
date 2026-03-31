359ec6edcd262bd48964209f1878889ad49b5255
Aisin_Lychee <95344822+yuan089@users.noreply.github.com>
2026-03-31 14:19:36 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+from langbot_plugin.api.definition.plugin import BasePlugin
+
+class CustomSearchPlugin(BasePlugin):
+    async def initialize(self) -> None:
+        pass
+
+    def __del__(self) -> None:
+        pass
